f0000c75e9f963030ff2851bca8bf29dfb5e08e6
sdrtba <sdrtba@yandex.ru>
2025-04-10 15:20:41 +0400

    add Base database

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,11 @@
 import sqlalchemy
+from sqlalchemy.ext import declarative
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./database.db"
+
+engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative.declarative_base()
