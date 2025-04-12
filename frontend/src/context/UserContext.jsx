fc11aef214494e8877ba6fc80735aee138fd635c
sdrtba <sdrtba@yandex.ru>
2025-04-12 17:19:01 +0400

    add userContext

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useState, useEffect } from 'react'
+
+export const UserContext = createContext()
+
+export const UserProvider = (props) => {
+    const [token, setToken] = useState(localStorage.getItem("token"))
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const requestOptions = {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + token,
+                },
+            }
+
+            const response = await fetch("/api/users/me", requestOptions)
+
+            if (!response.ok) {
+                setToken(null)
+            }
+            localStorage.setItem("token", token)
+        }
+
+        fetchUser()
+    }, [token]);
+
+    return (
+        <UserContext.Provider value={[token, setToken]}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
