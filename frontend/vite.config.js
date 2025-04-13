294398fcb4c234c4a00dc210d57c53001b8fcd76
sdrtba <sdrtba@yandex.ru>
2025-04-13 12:00:30 +0400

    fix nginx.conf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       '/api': {
-//         target: "http://213.108.23.238",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// })
-
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -31,7 +17,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: "http://213.108.23.238",
-        changeOrigin: false,
+        changeOrigin: true,
       },
     },
   },
