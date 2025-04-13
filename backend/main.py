e0285a3bf06f708eb34b27facec7401596faeeca
sdrtba <sdrtba@yandex.ru>
2025-04-13 11:28:51 +0400

    fixes

--- backend/main.py
+++ backend/main.py
@@ -73,4 +73,4 @@ async def get_api():
     return {"message": "Welcome to the API"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
