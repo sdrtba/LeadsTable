35740d8c154aa8dfecfd67c03ef45a1a4ad49c84
sdrtba <sdrtba@yandex.ru>
2025-04-13 11:32:44 +0400

    fix config.py

--- backend/config.py
+++ backend/config.py
@@ -19,6 +19,6 @@ class Settings(BaseSettings):
     def DATABASE_URL_psycopg(self):
         return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file="../.env")
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
