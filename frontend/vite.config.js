02cd8ad0de8298036ff0ab9da448b0798e7cc87b
sdrtba <sdrtba@yandex.ru>
2025-04-13 11:46:30 +0400

    test vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,36 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: "http://213.108.23.238",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// })
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: "http://213.108.23.238",
+        target: "http://localhost:8000",
         changeOrigin: true,
+      },
+    },
+  },
+  preview: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: "http://213.108.23.238",
+        changeOrigin: false,
       },
     },
   },
